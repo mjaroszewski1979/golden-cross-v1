ab55571f7537c7d8b2ebecba5f45fcfd59e036ac
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-03 17:23:45 +0200

    Update tests.py
    Added comments for sphinx docs

--- gold/tests.py
+++ gold/tests.py
@@ -11,10 +11,18 @@ from .models import Bitcoin
 from .views import index, success
 
 
-# Testing shop app
+# Testing gold app
 class GoldTest(TestCase):
+    """
+    Test case for the gold app functionalities, including URL resolutions, view responses, and model behaviors.
+    """
 
     def setUp(self):
+        """
+        Set up the initial conditions for the tests.
+        This method initializes a test client and creates a Bitcoin instance.
+        """
+        
         self.client = Client()
         self.bitcoin = Bitcoin(
             title = 'btc-usd',
@@ -32,16 +40,30 @@ class GoldTest(TestCase):
 
 
     def test_index_url_is_resolved(self):
+        """
+        Test if the URL for the index view is resolved correctly.
+        """
+        
         url = reverse('index')
         self.assertEquals(resolve(url).func, index)
 
     def test_index_get(self):
+        """
+        Test the GET request to the index view.
+        This method checks the response status, template used, and context.
+        """
+        
         response = self.client.get(reverse('index'))
         self.assertContains(response, 'Digital Gold |  Home', status_code=200)
         self.assertTemplateUsed(response, 'index.html')
         self.assertIsNotNone(response.context['bitcoin'])
 
     def test_index_post(self):
+        """
+        Test the POST request to the index view.
+        This method checks the response status and content after posting data.
+        """
+        
         data={
             'email' : 'mjtrading@gmail.com'
         }
@@ -50,20 +72,39 @@ class GoldTest(TestCase):
         self.assertTrue(b'Thank you for your email.' in response.content)
 
     def test_success_url_is_resolved(self):
+        """
+        Test if the URL for the success view is resolved correctly.
+        """
+        
         url = reverse('success')
         self.assertEquals(resolve(url).func, success)
 
     def test_success_get(self):
+        """
+        Test the GET request to the success view.
+        This method checks the response status and template used.
+        """
+        
         response = self.client.get(reverse('success'))
         self.assertContains(response, 'Digital Gold |  Success', status_code=200)
         self.assertTemplateUsed(response, 'success.html')
 
     def test_handler404(self):
+         """
+        Test the 404 handler for a non-existent URL.
+        This method checks the response status and template used.
+        """
+        
         response = self.client.get('/some_url/')
         self.assertContains(response, 'Digital Gold |  Page not found', status_code=404)
         self.assertTemplateUsed(response, '404.html')
 
     def test_bitcoin_model(self):
+        """
+        Test the Bitcoin model.
+        This method creates a new Bitcoin instance, saves it, and verifies its properties and count in the database.
+        """
+        
         time_now = datetime.datetime.now(tz=timezone.utc)
         btc_obj = Bitcoin(
             title = 'btc-pln',
